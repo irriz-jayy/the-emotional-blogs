2102ce909df73fd28bc179abb5684cd4125cfa08
irriz-jayy <jayfelix789@gmail.com>
2023-03-06 18:59:22 +0300

    Added image

--- the-emotional-blogs/src/components/Login.js
+++ the-emotional-blogs/src/components/Login.js
@@ -3,11 +3,17 @@ import "../App.css";
 const Login = () => {
   return (
     <div className="container">
-      <div className="image">
-        <h2>Image</h2>
-      </div>
-      <div className="login-form">
-        <h2>hey</h2>
+      <div className="form-container">
+        <div className="login-image">
+          <img
+            src="https://raw.githubusercontent.com/irriz-jayy/images/main/logo.png"
+            alt="login image"
+            className="imagel"
+          />
+        </div>
+        <div className="login-form">
+          <h2>hey</h2>
+        </div>
       </div>
     </div>
   );
