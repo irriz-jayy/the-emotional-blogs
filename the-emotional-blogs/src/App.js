521e4836670e2a1d993434548b1a1ff1b0796b78
irriz-jayy <jayfelix789@gmail.com>
2023-03-06 22:04:08 +0300

    Added home

--- the-emotional-blogs/src/App.js
+++ the-emotional-blogs/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Homepage />} />
       </Routes>
       {/* <Login /> */}
     </div>
