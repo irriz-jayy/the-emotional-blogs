6221767a9b83686a4b2a9971999c55e3b2a0f330
irriz-jayy <jayfelix789@gmail.com>
2023-03-06 22:27:08 +0300

    Added navbar

--- the-emotional-blogs/src/components/Navbar.js
+++ the-emotional-blogs/src/components/Navbar.js
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="navbar">
+      <div className="logo-img">
+        <img
+          src="https://raw.githubusercontent.com/irriz-jayy/images/main/logo.png"
+          height="70"
+          alt="logo image"
+        />
+        <div className="navbar-nav">
+          <Link to="#">Home</Link>
+          <Link to="#">New</Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
