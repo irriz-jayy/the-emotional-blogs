d5811b9f1cb6069ef189271104139d552004519d
irriz-jayy <jayfelix789@gmail.com>
2023-03-06 18:05:42 +0300

    Added login

---
+++ the-emotional-blogs/src/components/Login.js
@@ -0,0 +1,16 @@
+import "../App.css";
+
+const Login = () => {
+  return (
+    <div className="container">
+      <div className="image">
+        <h2>Image</h2>
+      </div>
+      <div className="login-form">
+        <h2>hey</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
