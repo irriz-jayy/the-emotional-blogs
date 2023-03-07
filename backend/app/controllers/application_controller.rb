4bf182406abfc096ab92de2d4cdf76a7f9bf3a8f
irriz-jayy <jayfelix789@gmail.com>
2023-03-07 17:23:39 +0300

    Added application controller

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
-require_relative 'blogs_controller.rb'
-require_relative 'authors_controller.rb'
-
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
   get "/" do
     {message: "Hello"}.to_json
-end 
+  end 
+end
