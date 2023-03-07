3abe97b1d99fdd89acd983ce1b9ea050d0d8233c
irriz-jayy <jayfelix789@gmail.com>
2023-03-07 17:36:14 +0300

    Added seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,5 +1,33 @@
 require_relative '../config/environment'
 
+puts "Creating authors..."
+
+2.times do
+    User.create(name: Faker::Name.name)
+end
+
+puts "Authors created successfully"
+
 puts "Creating blogs..."
 
+20.times do
+    Blog.create(
+    title: Faker::Book.title,
+    content:Faker::Lorem.paragraph_by_chars(number: 300),
+    user_id: rand(User.first.id..User.last.id)
+)
+end
+
 puts "Blogs created successfully"
+
+puts "Creating comments..."
+
+5.times do
+    Comment.create(
+        comment: Faker::Lorem.paragraph_by_chars(number: 100)
+        user_id: rand(User.first.id..User.last.id)
+        blog_id: rand(Blog.first.id..Blog.last.id)
+    )
+end
+
+puts "Comments created successfully"
