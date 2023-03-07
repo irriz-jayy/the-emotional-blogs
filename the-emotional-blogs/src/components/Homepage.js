d35de1e33962707644b1c55f74bd5a6d2b19ac10
irriz-jayy <jayfelix789@gmail.com>
2023-03-07 17:14:45 +0300

    Changes in rake file

--- the-emotional-blogs/src/components/Homepage.js
+++ the-emotional-blogs/src/components/Homepage.js
@@ -6,15 +6,15 @@ const Homepage = () => {
     <div className="home-container">
       <Navbar />
       <div class="card text-center">
-        <div class="card-header">Featured</div>
+        <div class="card-header">Author</div>
         <div class="card-body">
-          <h5 class="card-title">Special title treatment</h5>
+          <h5 class="card-title">Title</h5>
           <p class="card-text">
             With supporting text below as a natural lead-in to additional
             content.
           </p>
           <a href="#" class="btn btn-primary">
-            Go somewhere
+            View
           </a>
         </div>
         <div class="card-footer text-muted">2 days ago</div>
