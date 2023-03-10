8f951d407ab31e1f9587e575a715070440cf99d7
irriz-jayy <jayfelix789@gmail.com>
2023-03-10 12:28:35 +0300

    Added routing

--- the-emotional-blogs/src/App.js
+++ the-emotional-blogs/src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import Login from "./components/Login";
+import About from "./components/About";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Homepage/>}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
--- the-emotional-blogs/src/components/Navbar.js
+++ the-emotional-blogs/src/components/Navbar.js
@@ -3,10 +3,10 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
+  { name: "Home", href: "/", current: true },
+  { name: "View blog", href: "#", current: false },
+  { name: "New blog", href: "#", current: false },
+  { name: "About", href: "/about", current: false },
 ];
 
 function classNames(...classes) {
